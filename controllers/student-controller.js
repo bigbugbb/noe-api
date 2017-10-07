e0cbd2b02879ee9dcad4bcacb7587cf875a81af6
bigbug <bigbugbb@gmail.com>
2017-10-07 16:46:33 +0800

    Add basic CRUD to student api

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -15,10 +15,77 @@ router.post('/students', authenticate, (req, res) => {
   // const body = _.pick(req.body, ['firstname', 'lastname']);
   const student = new Student(req.body);
 
-  student.save().then(() => {
-    res.send(student);
+  student.save().then((doc) => {
+    res.send(doc);
   }).catch((e) => {
     res.status(400).send(e);
+  });
+});
+
+router.get('/students', authenticate, (req, res) => {
+  Student.find().then((students) => {
+    res.send({students});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.get('/students/:id', authenticate, (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Student.findOne({
+    _id: id
+  }).then((student) => {
+    if (!student) {
+      return res.status(404).send();
+    }
+
+    res.send({student});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.patch('/students/:id', authenticate, (req, res) => {
+  const id = req.params.id;
+  const body = _.omit(req.body, ['_id', 'userId']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Student.findByIdAndUpdate({_id: id}, {$set: body}, {new: true}).then((student) => {
+    if (!student) {
+      return res.status(404).send();
+    }
+
+    res.send({student});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+router.delete('/students/:id', authenticate, (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Student.findOneAndRemove({
+    _id: id
+  }).then((student) => {
+    if (!student) {
+      return res.status(404).send();
+    }
+
+    res.send({student});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
