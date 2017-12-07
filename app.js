24624094f102316f496844cc5f9cdbd0def87565
bigbugbb <bigbugbb@gmail.com>
2017-12-07 17:00:50 +0800

    Integrate socket.io service and refactor folder structure

--- app.js
+++ app.js
@@ -1,19 +1,2 @@
-const config = require('./config/config');
-const express = require('express');
-const app = express();
-const cors = require('./middleware/cors');
-const bodyParser = require('body-parser');
-
-const models = [
-  'user', 'student', 'school', 'company', 'business', 'order'
-];
-
-app.use(cors());
-app.use(bodyParser.json({ limit: '1mb' }));
-app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
-
-models.forEach(model => {
-  app.use('/api/v1', require(`./controllers/${model}-controller`));
-});
-
-module.exports = app;
+require('./services/api');
+require('./services/socketio');
---
+++ services/api.js
@@ -0,0 +1,23 @@
+const config = require('../config/config');
+const express = require('express');
+const app = express();
+const cors = require('../middleware/cors');
+const bodyParser = require('body-parser');
+
+const models = [
+  'user', 'student', 'school', 'company', 'business', 'order'
+];
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
+
+models.forEach(model => {
+  app.use('/api/v1', require(`../controllers/${model}-controller`));
+});
+
+const port = process.env.PORT;
+
+const server = app.listen(port, () => {
+  console.log(`Api server listening on port ${port}`);
+});
