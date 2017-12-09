0db5b358aa182e3283e98347c3f2cbd757c0c386
bigbugbb <bigbugbb@gmail.com>
2017-12-10 04:10:48 +0800

    Update socket communication with namespace and room

--- services/socketio.js
+++ services/socketio.js
@@ -4,6 +4,9 @@ const express = require('express'),
 socketio = require('socket.io'),
 socketioRedis = require('socket.io-redis');
 
+// const redis = require('redis'),
+// client = redis.createClient();
+
 const app = express();
 const port = process.env.SOCKET_PORT;
 const server = app.listen(port, () => {
@@ -14,20 +17,33 @@ const io = socketio(server);
 app.use(express.static('static'));
 
 io.adapter(socketioRedis({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT }));
-io.on('connection', (socket) => {
-  socket.on('room.join', (room) => {
-    console.log(socket.rooms);
+
+// middleware
+// io.use((socket, next) => {
+//   let token = socket.handshake.query.token;
+//   if (isValid(token)) {
+//     return next();
+//   }
+//   return next(new Error('authentication error'));
+// });
+
+const contact = io.of('/contact');
+
+contact.on('connection', (socket) => {
+  socket.on('join', (user, room) => {
     Object.keys(socket.rooms).filter((r) => r != socket.id)
       .forEach((r) => socket.leave(r));
-
     setTimeout(() => {
       socket.join(room);
-      socket.emit('event', 'Joined room ' + room);
-      socket.broadcast.to(room).emit('event', 'Someone joined room ' + room);
+      console.log(`user ${user} joined ${room}`);
     }, 0);
-  })
+  });
 
-  socket.on('event', (e) => {
-    socket.broadcast.to(e.room).emit('event', e.name + ' says hello!');
+  socket.on('message', (from, to, message) => {
+    contact.to(to).emit('message', from, message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnect');
   });
 });
